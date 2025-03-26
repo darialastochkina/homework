e72680cf69b9e130b633cff06d123991d80bbb58
Daria Lastochkina <darialastochkin@gmail.com>
2025-03-26 11:08:44 +0300

    12.2 hw

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,38 @@
+import logging
+import os
+
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+logger = logging.getLogger('app.masks')
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('logs/masks.log', mode='w')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str) -> str:
-    first_part = card_number[:6]
-    last_part = card_number[-4:]
-    masked_part = "** ****"
-    masked_number = f"{first_part[:4]} {first_part[4:6]}{masked_part} {last_part}"
-    return masked_number
+    """Функция для маскировки номера карты."""
+    logger.info("Запуск маскировки номера карты")
+    card_number = str(card_number)
+    block1 = card_number[:4]
+    block2 = card_number[4:6]
+    block3 = "**"
+    block4 = "****"
+    block5 = card_number[-4:]
+    mask_card = f"{block1} {block2}{block3} {block4} {block5}"
+    logger.info(f"Маскировка пройдена успешно - {mask_card}")
+    return mask_card
 
 
 def get_mask_account(account_number: str) -> str:
-    last_part = account_number[-4:]
-    masked_account = "**" + last_part
-    return masked_account
+    """Функция для маскировки номера счета."""
+    logger.info("Запуск маскировки номера счета")
+    block6 = account_number[-4:]
+    mask_number = f"**{block6}"
+    logger.info(f"Маскировка пройдена успешно - {mask_number}")
+    return mask_number
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,40 @@
+import logging
 import json
 import os
 from typing import List, Dict, Any
 
 
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+logger = logging.getLogger('app.utils')
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('logs/utils.log', mode='w')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(file_handler)
+
+
 def read_operations_from_json(file_path: str) -> List[Dict[str, Any]]:
     """Читает операции из JSON-файла и возвращает список транзакций."""
-    try:
-        if not os.path.exists(file_path):
-            return []
+    logger.info(f"Загрузка транзакций из файла {file_path}")
+    if not os.path.exists(file_path):
+        logger.error(f"Файл {file_path} не найден")
+        return []
 
-        with open(file_path) as file:
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
-        if not isinstance(data, list):
-            return []
-        return data
-    except (json.JSONDecodeError):
+        if isinstance(data, list):
+            logger.info(f"Файл {file_path} загружен успешно")
+            return data
+        logger.warning(f"Данные в файле {file_path} не являются списком")
+        return []
+    except json.JSONDecodeError:
+        logger.error(f"Ошибка декодирования JSON в файле {file_path}")
+        return []
+    except Exception as e:
+        logger.error(f"Неожиданная ошибка при чтении файла {file_path}: {str(e)}")
         return []
